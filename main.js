023f74d0ecf4355a83fbf6cf34e17befe1c02d07
gOK-m <71651713+gOK-m@users.noreply.github.com>
2021-05-04 16:39:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+    moustache = loadImage("https://i.postimg.cc/dQjR3m76/moustache1.png");
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(moustache, noseX, noseY, 80, 35);
+}
+function take_snapshot(){
+    save("moustache.png");
+}
+function modelLoaded(){
+    console.log("posenet is loaded!");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+    }
+}
